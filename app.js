3f580bb83643ee86b1ce304c56c0824869a39811
Rtroman14 <rtroman14@gmail.com>
2020-07-26 19:24:21 -0600

    test

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 require("dotenv").config();
 
-// Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
 });
 
-// All the room in the world for your code
+/* Add functionality here */
 
 (async () => {
-    // Start your app
+    // Start the app
     await app.start(process.env.PORT || 3000);
 
     console.log("⚡️ Bolt app is running!");
